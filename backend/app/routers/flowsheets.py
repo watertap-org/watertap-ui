cbaec56a125067568a910437d6087e6749b77413
Abdelrahman Elbashandy <aaelbashandy@lbl.gov>
2022-06-23 18:10:38 -0400

    Hooking watertap backend with FastAPI

--- backend/app/routers/flowsheets.py
+++ backend/app/routers/flowsheets.py
@@ -1,8 +1,10 @@
 import io
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
 
-from app.internal.flowsheet.flowsheet import Flowsheet
+from app.internal.flowsheet.flowsheet_interfaces_handler import flowsheet_interfaces_handler
+
+from watertap.ui.api import find_flowsheet_interfaces, WorkflowActions
 
 router = APIRouter(
     prefix="/flowsheets",
@@ -21,22 +23,31 @@ flowsheets_list = [
 
 @router.get("/")
 async def get_all():
-    return flowsheets_list
+    return flowsheet_interfaces_handler.get_list()
 
 @router.get("/{flowsheet_id}/config")
-async def get_config(flowsheet_id: str):
-    fs = Flowsheet(flowsheet_id)
-    config = fs.get_data()
-    return config
+async def get_config(flowsheet_id: int):
+    try:
+        fs = fs = flowsheet_interfaces_handler.get_interface(flowsheet_id)
+        config = fs.get_flowsheet_json()
+        return config
+    except KeyError:
+        raise HTTPException(status_code = 404, detail="Flowsheet not found")
 
 @router.get("/{flowsheet_id}/graph")
-async def get_graph(flowsheet_id: str):
-    fs = Flowsheet(flowsheet_id)
-    graph = fs.get_graph()
-    return StreamingResponse(io.BytesIO(graph), media_type="image/png")
+async def get_graph(flowsheet_id: int):
+    try:
+        fs = flowsheet_interfaces_handler.get_interface(flowsheet_id)
+        graph = fs.get_graph()
+        return StreamingResponse(io.BytesIO(graph), media_type="image/png")
+    except KeyError:
+        raise HTTPException(status_code = 404, detail="Flowsheet not found")
 
 @router.get("/{flowsheet_id}/solve")
-async def solve(flowsheet_id: str):
-    fs = Flowsheet(flowsheet_id)
-    results = fs.solve()
-    return results
+async def solve(flowsheet_id: int):
+    try:
+        fs = flowsheet_interfaces_handler.get_interface(flowsheet_id)
+        results = fs.solve()
+        return results
+    except KeyError:
+        raise HTTPException(status_code = 404, detail="Flowsheet not found")
