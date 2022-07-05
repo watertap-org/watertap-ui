2edb0d74889e0696e1f7c0a8e2e4efebdba227bc
Michael Pesce <michaelcpesce@gmail.com>
2022-07-05 18:02:51 -0400

    added initial component tests using react testing library

---
+++ electron/ui/src/tests/FlowsheetsListTable.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import FlowsheetsListTable from "Components/FlowsheetsListTable/FlowsheetsListTable"
+import test_data from '../../../../backend/data/flowsheets/fake/data.json'
+
+
+test('test flowsheets list table', () => {
+    render( <FlowsheetsListTable rows={[test_data]}/> )
+
+    expect(screen.getByRole('cell', {  name: /METAB treatment train/i}))
+    expect(screen.getByRole('link', {  name: /METAB treatment train/i}))
+    expect(screen.getByRole('button', { name: /edit project/i }))
+
+    screen.debug();
+
+})
