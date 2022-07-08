748b8a30edcbc80ccfbc9be3d3c3bb8c8dc4cc5f
Michael Pesce <michaelcpesce@gmail.com>
2022-07-08 13:31:51 -0400

    migrated tests data to data folder

--- electron/ui/src/tests/InputAccordion.test.js
+++ electron/ui/src/tests/InputAccordion.test.js
@@ -1,43 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import InputAccordion from "../components/InputAccordion/InputAccordion"
 import * as React from 'react'
+import mockSections from './data/InputAccordion.json'
 
 //mock props
 const key = ""
 
-const mockSections = {
-    "": {
-      "display_name": "",
-      "variables": {
-        "flow_vol": {
-          "value": {
-            "index": [
-              [
-                0
-              ]
-            ],
-            "value": [
-              0.0003286
-            ],
-            "bounds": [
-              [
-                null,
-                null
-              ]
-            ]
-          },
-          "display_name": "flow_vol",
-          "description": "Volumetric flowrate in feed",
-          "display_units": "m<sup>3</sup>/s",
-          "indices": [],
-          "scale_factor": 0,
-          "to_units": "",
-          "readonly": false,
-          "category": ""
-        },
-      }
-    }
-  }
 
 test('test input accordion', () => {
 
