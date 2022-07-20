cba3683cd6b5331d3740ab55df105e32303881e7
Michael Pesce <michaelcpesce@gmail.com>
2022-07-20 10:22:07 -0400

    added modal for saving config

--- electron/ui/src/views/FlowsheetConfig/ConfigOutput/ConfigOutput.js
+++ electron/ui/src/views/FlowsheetConfig/ConfigOutput/ConfigOutput.js
@@ -12,13 +12,30 @@ import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { saveConfig }  from '../../../services/output.service.js'
+import Modal from '@mui/material/Modal';
 
 
 export default function ConfigOutput(props) {
     let params = useParams(); 
     const { outputData, historyData } = props;
     const [ configName, setConfigName ] = useState("Configuration #"+props.historyData.length)
+    const [openSaveConfig, setOpenSaveConfig] = React.useState(false);
 
+    const handleOpenSaveConfig = () => setOpenSaveConfig(true);
+    const handleCloseSaveConfig = () => setOpenSaveConfig(false);
+
+    const modalStyle = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 300,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+      
 
     useEffect(()=>{   
         // console.log(outputData)
@@ -34,9 +51,11 @@ export default function ConfigOutput(props) {
         .then((data)=>{
             console.log('successfully saved config')
             historyData[historyData.length-1]['name'] = configName
+            handleCloseSaveConfig()
         })
         .catch((e) => {
             console.log('error saving config')
+            handleCloseSaveConfig()
         });
     }
 
@@ -99,17 +118,33 @@ export default function ConfigOutput(props) {
                 renderOutputAccordions()
             }
             <Grid item xs={12}> 
-                <TextField
-                    required
-                    variant="standard"
-                    id="margin-none"
-                    label="Config Name"
-                    value={configName}
-                    onChange={handleChangeConfigName}
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <Button onClick={handleSaveConfig}>Save Configuration</Button>
+                <Button onClick={handleOpenSaveConfig}>
+                    Save Configuration
+                </Button>
+                <Modal
+                    open={openSaveConfig}
+                    onClose={handleCloseSaveConfig}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Grid container sx={modalStyle} spacing={1}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                variant="standard"
+                                id="margin-none"
+                                label="Config Name"
+                                value={configName}
+                                onChange={handleChangeConfigName}
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={8}></Grid>
+                        <Grid item xs={4}>
+                            <Button onClick={handleSaveConfig} variant="contained">Save</Button>
+                        </Grid>
+                    </Grid>
+                </Modal>
             </Grid>
             </Grid>
         </>
