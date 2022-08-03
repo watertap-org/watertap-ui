8f5b2c963c6ff5a0ac446c575085a49158196ecc
Dan Gunter <dkgunter@lbl.gov>
2022-08-03 16:42:02 -0700

    fixed test errors

--- electron/ui/src/tests/FlowsheetsListTable.test.js
+++ electron/ui/src/tests/FlowsheetsListTable.test.js
@@ -1,13 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import FlowsheetsListTable from "../components/FlowsheetsListTable/FlowsheetsListTable"
-import test_data from '../../../../backend/data/flowsheets/fake/data.json'
+// import test_data from '../../../../backend/data/flowsheets/fake/data.json'
 import * as React from 'react'
 
-test('test flowsheets list table', () => {
-    render( <FlowsheetsListTable rows={[test_data]}/> )
+let test_data = [{
+    name: "METAB treatment train",
+    train: "Modular Encapsulated Two-stage Anaerobic Biological model",
+    id: 0
+}]
 
+test('test flowsheets list table', () => {
+    render( <FlowsheetsListTable rows={test_data}/> )
+
+    // These find the same <td> element with the name and link to the treatment train
     expect(screen.getByRole('cell', {  name: /METAB treatment train/i}))
     expect(screen.getByRole('link', {  name: /METAB treatment train/i}))
+    // This finds the description ('train') <td> element
+    expect(screen.getByRole('cell', { name: /anaerobic/i}))
+    // This finds the button
     expect(screen.getByRole('button', { name: /edit project/i }))
 
 })
