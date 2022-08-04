848d5da3141e96235004a324305d3b6e50e5b704
Michael Pesce <michaelcpesce@gmail.com>
2022-08-04 09:04:53 -0700

    migrated ui contents into electron directory; updated some start scripts

--- electron/main.js
+++ electron/main.js
@@ -3,83 +3,85 @@ const path = require('path')
 require('dotenv').config()
 
 const axios = require('axios').default;
-let spawn = require("child_process").spawn
+const isDev = require('electron-is-dev')
+const { spawn, execFile } = require("child_process")
 
+// Python server parameters
+const PY_HOST = "127.0.0.1";
+const PY_PORT = 8001;
+const UI_PORT = 3000;
+const PY_LOG_LEVEL = "info";
 let uiReady = false
-const serverURL = `http://localhost:${process.env.API_PORT}`
-const uiURL = `http://localhost:${process.env.UI_PORT}`
 
-const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-        width: 2000,
-        height: 1600,
-        // webPreferences: {
-        //     preload: path.join(__dirname, 'preload.js')
-        // }
-    })
+const serverURL = `http://localhost:${PY_PORT}`
+const uiURL = `http://localhost:${UI_PORT}`
 
-    mainWindow.webContents.openDevTools()
-    
-    mainWindow.loadURL(uiURL)
-    mainWindow.on('ready-to-show', () => {
-        if (!mainWindow) {
-          throw new Error('"mainWindow" is not defined');
-        }
-        if (process.env.START_MINIMIZED) {
-            mainWindow.minimize();
-        } else {
-            mainWindow.show();
-        }
-    });
+require('@electron/remote/main').initialize()
 
-    return mainWindow
+
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  })
+  win.webContents.openDevTools()
+  win.loadURL(
+    isDev
+      ? uiURL
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
 }
 
 const startServer = () => {
-    let backendProcess = spawn("uvicorn", 
+    if (isDev) {
+      backendProcess = spawn("uvicorn", 
         [
             "main:app",
             "--host",
             "127.0.0.1",
             "--port",
-            process.env.API_PORT,
-            "--reload"
+            PY_PORT
         ],
         {
             cwd: '../backend/app'
         }
     );
-
-    return backendProcess
-}
-
-const startUI = () => {
-    let frontendProcess = spawn("npm", 
+      console.log("Python process started in dev mode");
+    } else {
+      backendProcess = execFile(
+        path.join(__dirname, "../../py_dist/run_server/run_server"),
         [
-            'run',
-            "start-nob",
-        ],
-        {
-            cwd: 'ui',
-            env: {
-                ...process.env,
-                PORT: process.env.UI_PORT
-            }
-        }
-    );
-
-    return frontendProcess
+          "--host",
+          PY_HOST,
+          "--port",
+          PY_PORT,
+          "--log-level",
+          PY_LOG_LEVEL,
+        ]
+      );
+      console.log("Python process started in built mode");
+    }
+    return backendProcess;
 }
+    
+
 
 app.whenReady().then(() => {
     // Entry point
 
     let serverProcess = startServer()
-    let uiProcess = startUI()
 
+    let noTrails = 0
+    
     // Start Window 
-    var startUp = (url, appName, spawnedProcess, successFn=null, maxTrials=50) => {
-        let noTrails = 0
+    var startUp = (url, appName, spawnedProcess, successFn=null, maxTrials=15) => {
+        
         axios.get(url).then(() => {
             console.log(`${appName} is ready at ${url}!`)
             if (successFn) {
@@ -88,7 +90,7 @@ app.whenReady().then(() => {
         })
         .catch(async () => {
             console.log(`Waiting to be able to connect ${appName} at ${url}...`)
-            await new Promise(resolve => setTimeout(resolve, 1000))
+            await new Promise(resolve => setTimeout(resolve, 2000))
             noTrails += 1
             if (noTrails < maxTrials) {
                 startUp(url, appName, spawnedProcess, successFn, maxTrials)
@@ -99,8 +101,7 @@ app.whenReady().then(() => {
             }
         });
     };
-    startUp(serverURL, 'FastAPI Server', serverProcess)
-    startUp(uiURL, 'React App', uiProcess, createWindow)
+    startUp(serverURL, 'FastAPI Server', serverProcess, createWindow)
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
