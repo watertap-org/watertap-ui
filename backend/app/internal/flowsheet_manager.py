623b56b6d66cc04a0737a10eebf926a58a675dd2
Dan Gunter <dkgunter@lbl.gov>
2022-09-07 11:39:12 -0700

    graph_settings-issue53 (#54)
    * initial
    
    * x
    
    * a
    
    * b
    
    * missing diagram
    
    * missing diagram
    
    * tweak
    
    * tweak
    
    * Fixed info API and some tests
    
    * put api example used for tests in the test/ tree

--- backend/app/internal/flowsheet_manager.py
+++ backend/app/internal/flowsheet_manager.py
@@ -1,4 +1,10 @@
 # stdlib
+import sys
+
+if sys.version_info < (3, 10):
+    from importlib_resources import files
+else:
+    from importlib.resources import files
 from pathlib import Path
 import shutil
 import time
@@ -18,35 +24,33 @@ import idaes.logger as idaeslog
 _log = idaeslog.getLogger(__name__)
 _log.setLevel(logging.DEBUG)
 
+
 class FlowsheetInfo(BaseModel):
     """Information about a flowsheet."""
+
     # static information
     id_: str
     name: str
     description: str = ""
+    module: str = ""
     # current status of flowsheet
     built: bool = False
-    updated: float = 0  # time last updated (including built)
+    ts: float = 0  # time last updated (including built)
 
     # Make sure name is lowercase
     @validator("name")
     def normalize_name(cls, v: str, values):
         return v.lower()
 
-    def set_built(self):
-        """Call this after (re)building the flowsheet."""
-        self.built = True
-        self.updated = time.time()
-
-    def set_updated(self):
-        """Call this after updating values in the flowsheet."""
-        self.updated = time.time()
+    def updated(self, built: Optional[bool] = None):
+        self.ts = time.time()
+        if built is not None:
+            self.built = built
 
 
 class FlowsheetManager:
     """Manage the available flowsheets."""
 
-    DIAGRAM_FILE = "graph.png"
     HISTORY_DB_FILE = "history.json"
 
     def __init__(self, **kwargs):
@@ -56,7 +60,6 @@ class FlowsheetManager:
             **kwargs: Passed as keywords to :class:`AppSettings`.
         """
         self.app_settings = AppSettings(**kwargs)
-        self.app_settings.create_data_basedir()
         self._objs, self._flowsheets = {}, {}
         for package in self.app_settings.packages:
             _log.debug(f"Collect flowsheet interfaces from package '{package}'")
@@ -73,11 +76,15 @@ class FlowsheetManager:
                 id_ = module_name
                 export = obj.fs_exp  # exported flowsheet
                 info = FlowsheetInfo(
-                    id_=id_, name=export.name, description=export.description
+                    id_=id_,
+                    name=export.name,
+                    description=export.description,
+                    module=id_,
                 )
                 self._flowsheets[id_] = info
                 self._objs[id_] = obj
-                self._add_data_dir(id_)
+                path = self.get_flowsheet_dir(id_)
+                path.mkdir(exist_ok=True)
 
         # Connect to history DB
         path = self.app_settings.data_basedir / self.HISTORY_DB_FILE
@@ -92,28 +99,37 @@ class FlowsheetManager:
         Returns:
             Path to data directory
         """
-        # replace non-alphanumeric chars with underscore
-        id_filename = str(id_)
-        return self.app_settings.data_basedir / id_filename
-
-    def _add_data_dir(self, id_: str):
-        path = self.get_flowsheet_dir(id_)
-        if not path.exists():
-            path.mkdir()
-        # XXX: remove this when we have a real way to get the diagrams
-        src = self.get_flowsheet_dir("fake") / self.DIAGRAM_FILE
-        dst = path / self.DIAGRAM_FILE
-        shutil.copyfile(src, dst)
+        return self.app_settings.data_basedir / self.get_info(id_).module
 
     @property
     def flowsheets(self) -> List[FlowsheetInfo]:
         return list(self._flowsheets.values())
 
-    def get_diagram(self, id_: str):
-        _ = self.get_obj(id_)  # verifies the flowsheet exists
-        path = self.get_flowsheet_dir(id_) / self.DIAGRAM_FILE
-        with path.open(mode="rb") as f:
-            data = f.read()
+    def get_diagram(self, id_: str) -> bytes:
+        """Get diagram for a given flowsheet.
+
+        Args:
+            id_: Flowsheet identifier
+
+        Returns:
+            Diagram image data, which will be empty if none is found.
+
+        Raises:
+            HTTPException: if the flowsheet itself cannot be found
+        """
+        data = b""
+        info = self.get_info(id_)
+
+        dot = info.module.rfind(".")
+        if dot < 0:
+            _log.error(f"Cannot get diagram for a package ({info.module})")
+        else:
+            p, m = info.module[:dot], info.module[dot + 1 :]
+            try:
+                data = files(p).joinpath(f"{m}.png").read_bytes()
+            except (FileNotFoundError, IOError) as err:
+                _log.error(f"Cannot read diagram for flowsheet '{id_}': {err}")
+
         return data
 
     def get_obj(self, id_: str) -> FlowsheetInterface:
@@ -182,10 +198,11 @@ class FlowsheetManager:
             Exact name used in DB record
         """
         info = self.get_info(id_)
+        info.ts = time.time()
         fs_q = tinydb.Query()
         _log.debug(f"Saving/replacing name='{name}' for id='{id_}'")
         self._histdb.upsert(
-            {"name": name, "id_": id_, "ts": info.updated, "data": data},
+            {"name": name, "id_": id_, "ts": info.ts, "data": data},
             (fs_q.id_ == id_) & (fs_q.name == name),
         )
         return name
--- backend/tests/app/internal/test_flowsheet_manager.py
+++ backend/tests/app/internal/test_flowsheet_manager.py
@@ -6,6 +6,8 @@ import pytest
 from app.internal import flowsheet_manager as fm
 from fastapi import HTTPException
 import logging
+import time
+
 
 # logging.getLogger("idaes.app.internal.flowsheet_manager").setLevel(logging.DEBUG)
 # logging.getLogger("idaes.watertap.ui.fsapi").setLevel(logging.DEBUG)
@@ -13,11 +15,14 @@ import logging
 
 @pytest.fixture(scope="module")
 def mgr():
-    return fm.FlowsheetManager(packages=["watertap", "examples"])
+    return fm.FlowsheetManager(packages=["watertap", "tests.app.internal.examples"])
 
 
-def one_id(fm):
-    return list(fm._flowsheets.keys())[0]
+def one_id(fm, filter_func=None):
+    for key in fm._flowsheets:
+        if filter_func is None or filter_func(key):
+            return key
+    return None
 
 
 @pytest.mark.unit
@@ -32,9 +37,8 @@ def test_mgr_construct(mgr):
 
 @pytest.mark.unit
 def test_mgr_get_flowsheet_dir(mgr: fm.FlowsheetManager):
-    name = uuid4().hex  # assure it doesn't exist, which is OK
-    d = mgr.get_flowsheet_dir(name)
-    assert str(d).endswith(name)
+    with pytest.raises(HTTPException):
+        mgr.get_flowsheet_dir(uuid4().hex)
 
 
 @pytest.mark.unit
@@ -44,8 +48,31 @@ def test_mgr_flowsheets_property(mgr: fm.FlowsheetManager):
 
 @pytest.mark.unit
 def test_mgr_get_diagram(mgr: fm.FlowsheetManager):
-    d = mgr.get_diagram(one_id(mgr))
+    no_diagram = "nodiagram"
+    print(f"All flowsheets: {mgr._flowsheets.keys()}")
+
+    # get an examples module
+    mod_id = one_id(
+        mgr, filter_func=lambda k: (k.startswith("tests") and no_diagram not in k)
+    )
+    print(f"Getting diagram: {mod_id}")
+    d = mgr.get_diagram(mod_id)
     assert d
+    # known size of "diagram"
+    assert 18000 < len(d) < 19000
+
+    # module has no API fails
+    with pytest.raises(HTTPException):
+        mgr.get_diagram("watertap.core.wt_database")
+
+    # raw pkg fails
+    with pytest.raises(HTTPException):
+        mgr.get_diagram("examples")
+
+    mod_id2 = mod_id.replace("api_example", f"api_example_{no_diagram}")
+    print(f"Getting diagram: {mod_id2}")
+    b = mgr.get_diagram(mod_id2)
+    assert len(b) == 0
 
 
 @pytest.mark.unit
@@ -71,3 +98,24 @@ def test_mgr_history(mgr: fm.FlowsheetManager):
     data = mgr.get_flowsheet_data(id_=fsid, name=save_name)
     assert len(data) == 1
     assert data[0] == data2
+
+
+@pytest.mark.unit
+def test_info():
+    info = fm.FlowsheetInfo(id_="an-id", name="A name")
+    assert info.id_ == "an-id"
+    assert info.name == "a name"  # normalized to lowercase
+    assert info.description == ""
+    assert info.module == ""
+    assert info.built is False
+    assert info.ts == 0
+
+    info.updated()
+    assert info.ts > 0
+    assert info.built is False
+
+    ts = info.ts
+    time.sleep(0.5)
+    info.updated(built=True)
+    assert info.built is True
+    assert ts < info.ts
