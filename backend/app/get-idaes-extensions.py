b965abcba9db50b368921ba0d7473998f775875c
MichaelPesce <michaelcpesce@gmail.com>
2022-09-21 17:04:34 -0400

    hopefully fixed ssl error

--- backend/app/get-idaes-extensions.py
+++ backend/app/get-idaes-extensions.py
@@ -1,8 +1,10 @@
 import sys
 import os
+import certifi
 from idaes.util.download_bin import download_binaries
 from idaes.config import default_binary_release
 from pathlib import Path
+
 
 extensions_dir = Path.home() / ".watertap" / ".idaes"
 def check_for_extensions():
@@ -14,13 +16,18 @@ def check_for_extensions():
 def get_extensions():
     print('inside get extensions')
     try:
+        print('setting requests_ca_bundle and ssl_cert_file')
+        os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+        os.environ["SSL_CERT_FILE"] = certifi.where()
+    except Exception as e:
+        print(f'unable to set requests_ca_bundle and ssl_cert_file:\n{e}')
+    try:
         if(sys.platform == "darwin"):
             #XXX doesnt work on idaes 2.0.0 - unsupported darwin-x86_64
             print('mac')
             print('trying to download binaries')
             download_binaries(url=f'https://idaes-extensions.s3.us-west-1.amazonaws.com/')
             print(f'extensions have been gotten, making directory')
-
         else:
             print('not mac')
             print(f'trying to download binaries')
