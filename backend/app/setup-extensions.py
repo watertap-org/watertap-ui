bc84afd1b1838cf77fa9d77c8e841b1e3fe52ed8
MichaelPesce <michaelcpesce@gmail.com>
2022-10-03 09:25:46 -0400

    updated linux installer

--- backend/app/setup-extensions.py
+++ backend/app/setup-extensions.py
@@ -25,17 +25,11 @@ def get_idaes_extensions():
             print(f'get idaes extensions successful, making directory')
         elif(sys.platform == "linux"):
             print('linux')
-            try:
-                print(f'setting requests_ca_bundle and ssl_cert_file to certifi.where(): {certifi.where()}')
-                os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
-                os.environ["SSL_CERT_FILE"] = certifi.where()
-            except Exception as e:
-                print(f'unable to set requests_ca_bundle and ssl_cert_file:\n{e}')
-            print(f'trying to download binaries')
-            from idaes.util.download_bin import download_binaries
-            from idaes.config import default_binary_release
-            download_binaries(release=default_binary_release)
-            print(f'extensions have been gotten')
+            idaes_src = Path(os.path.dirname(os.path.realpath(__file__))) / 'extensions' / '.idaes'
+            idaes_dst = Path.home() / ".idaes"
+            print(f'moving binaries from {idaes_src} to {idaes_dst}')
+            copytree(idaes_src,idaes_dst,dirs_exist_ok=True)
+            print(f'get idaes extensions successful, making directory')
         else:
             print('windows')
             try:
