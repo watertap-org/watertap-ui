759eb32a6279af059e8ccc5a40775ce3a32f758c
MichaelPesce <35114629+MichaelPesce@users.noreply.github.com>
2023-03-23 06:44:48 -0500

    Functionality to unfix input variables, display degrees of freedom (#78)
    * fixed graph css
    
    * Add "delete saved config" button, data versioning (#68) (#17)
    
    * added dropdown for choosing previous configurations
    
    * added delete button, needs functionality
    
    * fixed graph css
    
    * added delete functionality
    
    * added delete confirmation before deleting saved config
    
    * added try/catch so that incorrect data versions wont break screen
    
    * added data versioning
    
    * added errorbar component
    
    * removed second save and solve buttons from input page
    
    * v5->v6
    
    * added yaml configs
    
    * updated error handling for graph fetching
    
    * excluded psutil, lxml from electron build
    
    * removed import of 'this'; updated local backend start script to run on 127.0.0.1
    
    * updated pyinstaller config to include all .yaml files in build
    
    * merge main into display-updates branch (#16)
    
    * Remove redundant flowsheets entry points now defined in watertap
    
    * Parametrize flowsheet_id fixture for REST endpoints tests
    
    * Add step to display environment in failing CI job
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>
    Co-authored-by: Keith Beattie <ksbeattie@lbl.gov>
    
    * updated python tests to include data version number
    
    * updated component test data
    
    * changed first save button -> update flowsheet
    
    * added return to list page button
    
    * updated e2e tests
    
    * ensure package-lock.json is up to date
    
    * updated extensions installer to include linux
    
    * updated linux installer
    
    * linux configuration
    
    * updated linux configuration; updated pytest test_missing
    
    * updated pytest/test_update: ensure that we dont try to update readonly variables
    
    * updated workflow: updated path to cypress artifacts
    
    * updated readme
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>
    Co-authored-by: Keith Beattie <ksbeattie@lbl.gov>
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>
    Co-authored-by: Keith Beattie <ksbeattie@lbl.gov>
    
    * added bounds component for free variables; free variable functionality relies on changes ive made on my fork in watertap
    
    * updated workflow to use my fork of watertap for testing
    
    * updated workflow: trigger test on any push
    
    * fixed typo in workflow
    
    * added landing page; right now the entire page is just an image so it looks terrible when stretched
    
    * updated component test
    
    * updated splash page to use correct background image, updated text and color
    
    * added a wait to e2e test to ensure flowsheet loads
    
    * empty strings for bounds input -> null; added pyomo import for pyinstaller hook
    
    * updated build instructions in readme
    
    * updated build instructions in readme
    
    * added display of dof on config input
    
    * added check for undefined fixed attribute
    
    * updated location of degrees of freedom
    
    ---------
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>
    Co-authored-by: Keith Beattie <ksbeattie@lbl.gov>

--- electron/ui/src/components/Boilerplate/Header/Header.js
+++ electron/ui/src/components/Boilerplate/Header/Header.js
@@ -4,25 +4,27 @@ import logo from "../../../assets/nawi-logo-color.png";
 import { useNavigate } from "react-router-dom";
 import Button from '@mui/material/Button';
  
-export default function Header() {
+export default function Header(props) {
   let navigate = useNavigate();
   const handleNavigateHome = () => {
-      navigate("/", {replace: true})
+      navigate("/flowsheets", {replace: true})
   }
     return (
+      props.show && 
       <div id="Header">
-        <div  className="titlebar" > 
-            <div id="nawi_logo" style={{cursor:'pointer'}} onClick={handleNavigateHome}>
-              <img src={logo} alt="NAWI logo"/>
-            </div>
-          <div id="titlebar-name">
-            WaterTAP
-          </div>
-          <div  className="right" > 
-          <Button style={{ color:"white" }} onClick={handleNavigateHome}>Return to list page</Button>
-          </div>
-        </div> 
         
+         <div  className="titlebar" > 
+          <div id="nawi_logo" style={{cursor:'pointer'}} onClick={handleNavigateHome}>
+            <img src={logo} alt="NAWI logo"/>
+          </div>
+        <div id="titlebar-name">
+          WaterTAP
+        </div>
+        <div  className="right" > 
+        <Button style={{ color:"white" }} onClick={handleNavigateHome}>Return to list page</Button>
+        </div>
+      </div> 
+       
       </div>
     );
 }
