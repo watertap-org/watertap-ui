15a30172e5776c385fc67f19b4ef063080754413
MichaelPesce <michaelcpesce@gmail.com>
2023-05-25 08:54:07 -0500

    add handleupdatesamples prop

--- electron/ui/src/components/InputAccordion/InputAccordion.js
+++ electron/ui/src/components/InputAccordion/InputAccordion.js
@@ -34,6 +34,7 @@ export default function InputAccordion(props) {
                         handleUpdateDisplayValue={handleUpdateDisplayValue} 
                         handleUpdateFixed={handleUpdateFixed} 
                         handleUpdateBounds={handleUpdateBounds}
+                        handleUpdateSamples={handleUpdateSamples}
                     />
             
         });
