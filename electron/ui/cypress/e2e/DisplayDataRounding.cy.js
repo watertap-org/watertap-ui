f4b9b69900ffb08249d26fdef289e35fd3b23e9a
MichaelPesce <michaelcpesce@gmail.com>
2023-05-25 12:45:18 -0500

    updated e2e test

--- electron/ui/cypress/e2e/DisplayDataRounding.cy.js
+++ electron/ui/cypress/e2e/DisplayDataRounding.cy.js
@@ -9,7 +9,7 @@ describe('Display Data Rounding', () => {
         cy.wait (5000)
 
         //click flowsheet and wait for api response
-        var flowsheet_name = "amo 1690"
+        var flowsheet_name = "magprex"
         cy.intercept({
             method: "GET",
             url: "http://localhost:8001/flowsheets/**",
