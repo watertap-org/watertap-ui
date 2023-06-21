e6760ef9cbd5806cfff9b06f6f6723d3e3f506d9
MichaelPesce <michaelcpesce@gmail.com>
2023-06-21 08:15:07 -0500

    fixed bug with input values not changing

--- electron/ui/src/components/InputAccordion/InputAccordion.js
+++ electron/ui/src/components/InputAccordion/InputAccordion.js
@@ -8,7 +8,7 @@ import InputWrapper from "../InputWrapper/InputWrapper";
 export default function InputAccordion(props) {
     const { handleUpdateDisplayValue, handleUpdateFixed, handleUpdateBounds, handleUpdateSamples, data } = props;
     const [expanded1, setExpanded1] = useState('panel1'); 
-    // const [value, setValue] = useState("");
+    const [value, setValue] = useState("");
 
     useEffect(()=>{  
         // console.log("DDD:",data);
@@ -18,9 +18,9 @@ export default function InputAccordion(props) {
       setExpanded1(isExpanded ? panel : false);
     };
 
-    // const handleFieldChange = (event) => {
-    //     setValue(event.target.value);
-    // };
+    const handleFieldChange = (event) => {
+        setValue(event.target.value);
+    };
 
     const renderFields = () => {
         return Object.keys(data.input_variables).map((key)=>{
--- electron/ui/src/components/InputWrapper/InputWrapper.js
+++ electron/ui/src/components/InputWrapper/InputWrapper.js
@@ -5,7 +5,7 @@ import { TextField, InputAdornment, Tooltip, Grid, MenuItem, FormControl, Select
 export default function InputWrapper(props) {
     const { fieldData, handleUpdateDisplayValue, handleUpdateFixed, handleUpdateBounds, handleUpdateSamples } = props;
     // const [ disabled, setDisabled ] = useState(false)
-    // const [value, setValue] = useState("");
+    const [value, setValue] = useState("");
     const [ showBounds, setShowBounds ] = useState(!fieldData.fixed)
     const disabled = false
 
@@ -18,7 +18,7 @@ export default function InputWrapper(props) {
     }, [fieldData]);
 
     const handleFieldChange = (event) => {
-        // setValue(event.target.value);
+        setValue(event.target.value);
         fieldData.value = event.target.value;
         handleUpdateDisplayValue(event.target.id,event.target.value)
     };
