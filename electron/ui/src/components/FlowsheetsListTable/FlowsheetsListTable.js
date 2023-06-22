3ff8673952049dc581a02cc2d54eb6731e67434f
MichaelPesce <michaelcpesce@gmail.com>
2023-06-22 07:53:17 -0500

    added sorting to flowsheet lists table

--- electron/ui/src/components/FlowsheetsListTable/FlowsheetsListTable.js
+++ electron/ui/src/components/FlowsheetsListTable/FlowsheetsListTable.js
@@ -1,9 +1,14 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { useState } from 'react'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Icon } from '@mui/material'
 import { useNavigate } from "react-router-dom";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
  
 export default function FlowsheetsListTable(props) {
     let navigate = useNavigate();
+    const [ sortKey, setSortKey ] = useState("name")
+    const [ sortDirection, setSortDirection ] = useState("ascending")
     const handleFlowsheetClick = (id) => {
         navigate("/flowsheet/" + id + "/config", {replace: true})
     }
@@ -15,6 +20,8 @@ export default function FlowsheetsListTable(props) {
             },
             '&:last-child td, &:last-child th': { border: 0 }
             
+        },
+        listHeaderRow: {
         }
     }
 
@@ -31,18 +38,59 @@ export default function FlowsheetsListTable(props) {
         
     }
 
+    const handleSort = (key) => {
+        if(sortKey === key) {
+            if(sortDirection === 'ascending') setSortDirection('descending')
+            else if(sortDirection === 'descending') setSortDirection('ascending')
+        }
+        else {
+            setSortKey(key)
+            setSortDirection('ascending')
+        }
+    }
+
+    const sortRows = (rows) => {
+        if (sortKey) return (rows.sort(compare)) 
+        else return rows
+        
+    }
+
+    function compare( a, b ) {
+        if ( a[sortKey] < b[sortKey] ){
+            if(sortDirection === "ascending") return -1;
+            else return 1;
+        }
+        if ( a[sortKey] > b[sortKey] ){
+            if(sortDirection === "ascending") return 1;
+            else return -1;
+        }
+        return 0;
+    }
+      
+
     return (
         <TableContainer>
             <Table sx={{ minWidth: 700 }} aria-label="simple table">
-            <TableHead>
+            <TableHead sx={{margin: "50px"}}>
             <TableRow>
-                <TableCell>Flowsheet Name</TableCell>
-                <TableCell align="right">Last Run</TableCell> 
+                <TableCell >
+                    <span onClick={()=>handleSort('name')} style={{ cursor: "pointer"}}>
+                        Flowsheet Name
+                        <IconButton>{sortKey==="name" && (sortDirection === "ascending" ? <KeyboardArrowDownIcon/> : sortDirection === "descending" &&  <KeyboardArrowUpIcon/>)}</IconButton>
+                    </span>
+                </TableCell>
+                <TableCell align="right"> 
+                    <span onClick={()=>handleSort('last_run')} style={{ cursor: "pointer" }}>
+                        Last Run
+                        {sortKey==="name" && <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>}
+                        <IconButton>{sortKey==="last_run" && (sortDirection === "ascending" ? <KeyboardArrowDownIcon/> : sortDirection === "descending" &&  <KeyboardArrowUpIcon/>)}</IconButton>
+                    </span>
+                </TableCell> 
                 <TableCell></TableCell>
             </TableRow>
             </TableHead>
             <TableBody>
-            {props.rows.map((row) => (
+            {sortRows(props.rows).map((row) => (
                 <TableRow
                     key={row.name}
                     sx={styles.listRow}
