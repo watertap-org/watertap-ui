e143b33c747db6c1db037bca25964c2bd48a8296
MichaelPesce <michaelcpesce@gmail.com>
2023-10-16 14:08:13 -0500

    update component test

--- electron/ui/src/tests/NewFlowsheetDialog.test.js
+++ electron/ui/src/tests/NewFlowsheetDialog.test.js
@@ -11,21 +11,12 @@ const mockClose = () => {
 test('test new flowsheet dialog', () => {
     render( <NewFlowsheetDialog open={open} onClose={mockClose}></NewFlowsheetDialog> )
 
-    //test that all textboxes are present
-    screen.getByRole('textbox', {  name: /flowsheet name/i});
-    screen.getByRole('textbox', {  name: /has bypass/i});
-    screen.getByRole('textbox', {  name: /has desal feed/i});
-    screen.getByRole('textbox', {  name: /is twostage/i});
-    screen.getByRole('textbox', {  name: /has erd/i});
-    screen.getByRole('textbox', {  name: /nf type/i});
-    screen.getByRole('textbox', {  name: /nf base/i});
-    screen.getByRole('textbox', {  name: /ro type/i});
-    screen.getByRole('textbox', {  name: /ro base/i});
-    screen.getByRole('textbox', {  name: /ro level/i});
-
-    //test that cancel and create buttons are present
-    screen.getByRole('button', {  name: /cancel/i})
-    screen.getByRole('button', {  name: /create/i})
+    screen.getByRole('heading', {  name: /Upload a new flowsheet/i});
+    screen.getByRole('heading', {  name: /Drag and drop Model File or Browse.../i});
+    screen.getByRole('heading', {  name: /Drag and drop Export File or Browse.../i});
+    screen.getByRole('heading', {  name: /Drag and drop Diagram File or Browse.../i});
+    screen.getByRole('heading', {  name: /Drag and drop Data Files or Browse.../i});
+    screen.getByRole('button', {  name: /upload flowsheet/i})
 
     //screen.debug();
 })
