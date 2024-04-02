d4c2bbc04221ec9d01b398a6f9c1a85a5f3c5129
MichaelPesce <michaelcpesce@gmail.com>
2024-04-02 06:42:05 -0500

    move api calls from comparison table to comparison view

--- electron/ui/src/components/OutputComparisonChart/OutputComparisonChart.js
+++ electron/ui/src/components/OutputComparisonChart/OutputComparisonChart.js
@@ -15,19 +15,71 @@ export default function OutputComparisonChart(props) {
 
     }
 
+    useEffect(() => {
+        unpackData("line")
+    }, [outputData])
+
+    const unpackData = (plotType) => {
+        // console.log(outputData.outputData)
+        let keys = outputData.outputData.sweep_results.keys
+        if (plotType==="line"){ // line plot
+            let x = []
+            let ys = []
+            for (let i = 0; i < outputData.outputData.sweep_results.num_outputs; i++) {
+                ys.push([])
+            }
+            for (let each of outputData.outputData.sweep_results.values) {
+                x.push(Math.round(each[0] * 1000) / 1000)
+                
+                for(let i = 1; i < each.length; i++) {
+                    let out=null
+                    if (each[i]!==null){
+                        out = Math.round(each[i] * 1000) / 1000}
+                    ys[i-1].push(out)
+                }
+            }
+            let tempData = []
+            let keyIdx = 1
+            // for (let each of ys) {
+            //     if( keyIdx === yIndex){
+            //         let yName = `${outputData.outputData.sweep_results.headers[keyIdx]} (${outputData.outputData.model_objects[keys[keyIdx]].display_units})`
+            //         let tempTrace = {x: x, y: each, type:"scatter", name: yName}
+            //         tempData.push(tempTrace)
+                    
+            //     }
+            //     keyIdx+=1
+            // }
+            
+            // let xLabel = `${outputData.outputData.sweep_results.headers[0]} (${outputData.outputData.model_objects[keys[0]].display_units})`
+            // let yLabel = `${outputData.outputData.sweep_results.headers[yIndex]} (${outputData.outputData.model_objects[keys[yIndex]].display_units})`
+            let xLabel, yLabel
+            let tempLayout = {
+                xaxis: {
+                    title: {
+                        text: xLabel,
+                    },
+                },
+                yaxis: {
+                    title: {
+                        text: yLabel,
+                    },
+                    // type: 'log',
+                    // autorange: true,
+                },
+                width: 700,
+                height: 500,
+            };
+            // console.log('lineplot data: ')
+            // console.log(tempData)
+
+            setPlotData({data: tempData, layout:tempLayout})
+            setShowPlot(true)
+        } 
+    }
+
     
     return ( 
         <Grid container spacing={2}> 
-            {/* <Grid item xs={12}> 
-                <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                    <Tabs value={tabValue} onChange={handleTabChange} aria-label="tabs">
-                        <Tab label="Table View" />
-                        <Tab label="Chart View" /> 
-                    </Tabs>
-                </Box>
-                
-            </Grid> */}
-           
                 
             <Grid sx={{marginBottom:15, paddingBottom: 50}} item xs={12}>
                 {showPlot && 
@@ -40,7 +92,8 @@ export default function OutputComparisonChart(props) {
                 
                 }
             
-            </Grid>            
+            </Grid>
+
         </Grid>
     );
 }
--- electron/ui/src/views/FlowsheetConfig/ConfigOutput/OutputComparison.js
+++ electron/ui/src/views/FlowsheetConfig/ConfigOutput/OutputComparison.js
@@ -3,13 +3,87 @@ import { useParams } from "react-router-dom";
 import { Grid, Button, Box, Paper, Stack, Toolbar, Tabs, Tab } from '@mui/material';
 import OutputComparisonTable from "../../../components/OutputComparisonTable/OutputComparisonTable.js";
 import OutputComparisonChart from "../../../components/OutputComparisonChart/OutputComparisonChart.js";
+import { loadConfig, listConfigNames }  from '../../../services/output.service.js'
 
 export default function OutputComparison(props) {
+    let params = useParams(); 
     const { outputData } = props;
+    const [ pastConfigs, setPastConfigs ] = useState([])
+    const [ historyData, setHistoryData ] = useState([])
+    const [ historyDataOrganized, setHistoryDataOrganized ] = useState([])
     const [ tabValue, setTabValue ] = useState(0)
+
+    useEffect(() => {
+      if (historyData.length > 1) {
+        try{
+          organizeVariables()
+        } catch {
+          console.error("unable to organize variables")
+        }
+      }
+      
+    
+    }, [historyData])
+
+    useEffect(()=>{
+      listConfigNames(params.id, outputData.inputData.version)
+      .then(response => response.json())
+      .then((data)=>{
+        setPastConfigs(data)
+        var tempHistory = []
+        for (const config of data) {
+          loadConfig(params.id, config)
+          .then(response => response.json())
+          .then((data2)=>{
+            tempHistory.push({name: config.replaceAll('"',''), data:data2})
+            setHistoryData([...tempHistory])
+          }).catch((err)=>{
+              console.error("unable to get load config: ",err)
+          });
+      }
+      }).catch((err)=>{
+          console.error("unable to get list of config names: ",err)
+      })
+      
+    }, []);
 
     const handleTabChange = (event, newValue) => {
       setTabValue(newValue)
+    }
+
+    const organizeVariables = () => {
+      var tempHistory = []
+      for (const bvars of historyData) {
+        let var_sections = {}
+        let tempVariables = {}
+        let tempName = bvars.name
+        for (const [key, v] of Object.entries(bvars.data.outputData.model_objects)) {
+            
+            let catg
+            let is_input = v.is_input
+            let is_output = v.is_output
+            if (is_input) catg = v.input_category
+            if (is_output) catg = v.output_category
+            if (catg === null) {
+                catg = ""
+            }
+            if (!Object.hasOwn(var_sections, catg)) {
+                var_sections[catg] = {display_name: catg, variables: [], input_variables:[], output_variables:[]}
+            }
+            if (!Object.hasOwn(tempVariables, catg)) {
+              tempVariables[catg] = {variables: [], input_variables:[], output_variables:[]}
+          }
+          tempVariables[catg].variables.push(v)
+          var_sections[catg]["variables"] = [...tempVariables[catg].variables];
+            if(is_output) {
+              tempVariables[catg].output_variables.push(v)
+              var_sections[catg]["output_variables"] = [...tempVariables[catg].output_variables];
+            }
+        }
+        tempHistory.push({name: tempName, data: var_sections})
+        setHistoryDataOrganized([...tempHistory])
+      }
+      
     }
 
   return (
@@ -25,10 +99,15 @@ export default function OutputComparison(props) {
                 
             </Grid>
             {  tabValue === 0 &&
-                <OutputComparisonTable outputData={outputData}/>
+                <OutputComparisonTable
+                  historyData={historyDataOrganized}
+                />
             }
             {  tabValue === 1 &&
-                <OutputComparisonChart outputData={outputData}/>
+                <OutputComparisonChart 
+                  outputData={outputData}
+                  historyData={historyDataOrganized}  
+                />
             }
         </Box>
       
