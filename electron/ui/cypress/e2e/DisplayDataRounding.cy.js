5611d3621fe45ed32ce52e1f41ac030fc70ab8b5
Dan Gunter <dkgunter@lbl.gov>
2024-06-18 05:56:01 -0700

    moving repeated things to commands

--- electron/ui/cypress/e2e/DisplayDataRounding.cy.js
+++ electron/ui/cypress/e2e/DisplayDataRounding.cy.js
@@ -1,60 +1,39 @@
 describe('Display Data Rounding', () => {
     it('test that values rounded to and displayed as 0 retain their original value', () => {
+        // XXX: This doesn't really test anything different
+        // XXX: because the flowsheet was changed.
+
         let sc_count = 0
 
-        //load webpage
+        // Load webpage
         cy.visit('/')
         cy.screenshot(sc_count+'_loaded homepage')
         sc_count+=1
         cy.wait (5000)
 
-        //click on all flowsheets  
-        // cy.findByText("All Flowsheets").click()
-        // cy.screenshot(sc_count+'_clicked all flowsheets')
-        // sc_count+=1
-
-        const set_flowrate = () => {
-            let input_textbox_name = "Water mass flowrate";
-            let input_textbox = cy.findByRole('textbox', {  name: input_textbox_name});
-            input_textbox.click({force:true});
-            input_textbox = cy.findByRole('textbox', {  name: input_textbox_name});
-            input_textbox.type('1{backspace}{backspace}{backspace}{backspace}0.96');
-            cy.wait(500);
-        }
-
-        //click flowsheet and wait for api response
+        // Click flowsheet and wait for response
         var flowsheet_name = "RO with energy recovery flowsheet"
         cy.intercept({
             method: "GET",
             url: "http://localhost:8001/flowsheets/**",
         }).as("loadFlowsheet");
-    //  cy.findByRole('link', {  name: flowsheet_name}).click()
         cy.findByText(flowsheet_name).click()
         cy.wait("@loadFlowsheet");
         cy.screenshot(sc_count+'_loaded flowsheet')
         sc_count+=1
         cy.wait (5000)
-        //click on update and wait for api response
-        // cy.intercept({
-        //     method: "POST",
-        //     url: "http://localhost:8001/flowsheets/**",
-        // }).as("saveChanges");
-        // cy.findAllByRole('button', {  name: /update flowsheet/i}).eq(0).click()
-        // cy.wait("@saveChanges")
-        // cy.screenshot(sc_count+'_updated')
-        // sc_count+=1
 
-        set_flowrate();
-        
-        //click on solve and wait for api response
+        // Click on solve and wait for response
         cy.intercept({
             method: "POST",
             url: "http://localhost:8001/flowsheets/**",
         }).as("run");
-        cy.findAllByRole('button', {  name: /run/i}).eq(0).click()
+        cy.findAllByRole('button', {name: /run/i}).eq(0).click()
         cy.wait("@run").its('response.statusCode').should('eq', 200);
         cy.screenshot(sc_count+'_solved')
         sc_count+=1
+
+        // XXX: Test something??
 
         cy.screenshot(sc_count+'_end-test')
         sc_count+=1
--- electron/ui/cypress/support/commands.js
+++ electron/ui/cypress/support/commands.js
@@ -1,1 +1,35 @@
-import "@testing-library/cypress/add-commands"
+import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.add('load_homepage', () => {
+    cy.visit('/')
+    cy.screenshot('loaded homepage')
+})
+
+Cypress.Commands.add('load_ro_flowsheet', () => {
+    const flowsheet_name = 'RO with energy recovery flowsheet';
+    cy.intercept({
+        method: 'GET',
+        url: 'http://localhost:8001/flowsheets/**',
+    }).as('loadFlowsheet');
+    cy.findByText(flowsheet_name).click();
+    cy.wait('@loadFlowsheet');
+    cy.screenshot('loaded flowsheet');
+})
+
+Cypress.Commands.add('set_ro_flowrate', (value) => {
+    let input_textbox_name = 'Water mass flowrate';
+    let input_textbox = cy.findByRole('textbox', {name: input_textbox_name});
+    input_textbox.click({force:true});
+    input_textbox = cy.findByRole('textbox', {name: input_textbox_name});
+    input_textbox.type('{backspace}{backspace}{backspace}{backspace}' + value);
+    cy.wait(500);
+})
+
+Cypress.Commands.add('solve_flowsheet', () => {
+    cy.intercept({
+        method: 'POST',
+        url: 'http://localhost:8001/flowsheets/**',
+    }).as('run');
+    cy.findAllByRole('button', {name: /run/i}).eq(0).click();
+    cy.wait('@run');
+})
