7bad70011dd5d09f0baac42e14cb2d4071ded3b4
MichaelPesce <michaelcpesce@gmail.com>
2024-07-09 07:02:46 -0400

    combine e2e tests into one file

--- electron/ui/cypress/e2e/FlowsheetTesting.cy.js
+++ electron/ui/cypress/e2e/FlowsheetTesting.cy.js
@@ -1,4 +1,17 @@
-describe('Flowsheet Output Test', () => {
+describe('WaterTAP UI Testing', () => {
+    it('test flowsheets-list page', () => {
+        cy.load_flowsheets_list()
+
+        //locate heading
+        cy.findByRole('heading', {  name: /flowsheets/i})
+
+        //locate table headers
+        cy.findByRole('columnheader', {  name: /flowsheet name/i})
+        cy.findByRole('columnheader', {  name: /last run/i})
+        
+        cy.screenshot('end-test1')
+    })
+
     it('test output page when valid input is solved', () => {
         let sc_count = 1
 
@@ -54,4 +67,35 @@ describe('Flowsheet Output Test', () => {
         cy.screenshot(sc_count + '_end-test')
     })
 
+    it('test negative input for recovery rate', () => {
+        let sc_count = 1;
+
+        cy.load_flowsheets_list()
+        cy.load_ro_flowsheet()
+
+        cy.set_ro_flowrate('dfas');
+        cy.screenshot(sc_count + '_input1');
+        sc_count += 1;
+
+        cy.set_ro_flowrate(cy, '-10');
+        cy.screenshot(sc_count + '_input2');
+        sc_count += 1;
+
+        cy.screenshot(sc_count + '_end-test');
+    })
+
+    it('test that values rounded to and displayed as 0 retain their original value', () => {
+        // XXX: This doesn't really test anything different
+        // XXX: because the flowsheet was changed.
+
+        let sc_count = 0
+
+        cy.load_flowsheets_list()
+        cy.load_ro_flowsheet()
+        cy.solve_flowsheet()
+
+        // XXX: Test something??
+
+        cy.screenshot(sc_count+'_end-test')
+    })
 })
