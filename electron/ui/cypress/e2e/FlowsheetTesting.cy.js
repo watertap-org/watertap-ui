c709332b8666efb1fa9680b6641b47618bc65996
MichaelPesce <michaelcpesce@gmail.com>
2024-07-23 09:51:17 -0400

    cleanup

--- electron/ui/cypress/e2e/FlowsheetTesting.cy.js
+++ electron/ui/cypress/e2e/FlowsheetTesting.cy.js
@@ -4,41 +4,35 @@ describe('WaterTAP UI Testing', () => {
         cy.load_flowsheets_list()
         cy.screenshot('loaded flowsheet list page')
 
-        //locate heading
+        // verify that heading and table headers are present
         cy.findByRole('heading', {  name: /flowsheets/i})
-
-        //locate table headers
         cy.findByRole('columnheader', {  name: /flowsheet name/i})
         cy.findByRole('columnheader', {  name: /last run/i})
-        cy.screenshot('end-list-page-test')
-        
+        cy.screenshot('end-list-page-test')  
     })
 
     it('tests invalid inputs', () => {
         cy.load_flowsheets_list()
         cy.screenshot('loaded flowsheet list page')
         
+        // load flowsheet
         cy.load_flowsheet("RO with energy recovery flowsheet")
         cy.screenshot('loaded RO flowsheet');
 
+        // set invalid text input
         cy.set_ro_flowrate('dfas');
         cy.screenshot('invalid-text-input');
 
+        // solve flowsheet
         cy.solve_flowsheet()
+
+        // verify that error message is there
         cy.screenshot('error-message');
         cy.get('.error-message').should('be.visible')
 
-        cy.set_ro_flowrate('-10');
-        cy.screenshot('invalid-negative-input');
-
-        cy.solve_flowsheet()
-        cy.screenshot('error-message');
-
-        cy.get('.error-message').should('be.visible')
-
-        cy.screenshot('end-invalid-input-test');
-
+        // reset flowsheet
         cy.reset_flowsheet()
+        cy.screenshot('end-invalid-input-test');
     })
 
     it('tests logging panel', () => {
@@ -46,13 +40,13 @@ describe('WaterTAP UI Testing', () => {
         cy.wait(2000)
         cy.screenshot('loaded flowsheet list page')
         
+        // open logging panel
         cy.open_logging_panel()
         cy.screenshot('opened_logs')
 
-        // check that a log line of type info, warning, are present
+        // verify that a log line of type info and warning are present
         cy.get('.log-line').contains('INFO')
         cy.get('.log-line').contains('WARNING')
-
         cy.screenshot('end-logging-test')
     })
 
@@ -66,9 +60,9 @@ describe('WaterTAP UI Testing', () => {
         cy.downloadFile(modelFile,'cypress/downloads','testModelFile.py')
         cy.downloadFile(exportFile,'cypress/downloads','testModelFile_ui.py')
         
+        // click new flowsheet button
         cy.findByRole('button', {name: /new flowsheet +/i}).click()
         cy.wait(500)
-
         cy.screenshot("clicked new flowsheet")
         
         // select both files
@@ -80,15 +74,15 @@ describe('WaterTAP UI Testing', () => {
             action: 'drag-drop',
             force: true
         })
-
         cy.wait(500)
         cy.screenshot("dragged and dropped files")
 
+        // click upload
         cy.get('.upload-flowsheet-button').click()
-        
         cy.wait(1000)
         cy.screenshot("uploaded files")
 
+        // verify that flowsheet is now in list
         cy.get('.flowsheet-name').contains(/test custom flowsheet/i)
         cy.screenshot('end-new-flowsheet-test')
 
@@ -100,36 +94,35 @@ describe('WaterTAP UI Testing', () => {
             cy.wait(2000)
             cy.screenshot('loaded flowsheet list page')
             
+            // load flowsheet
             cy.load_flowsheet(flowsheet.name)
             cy.screenshot('loaded '+flowsheet.name);
             
+            // if model has build options, it must be manually built
             if (flowsheet.buildRequired) {
                 cy.build_flowsheet()
                 cy.screenshot("built "+flowsheet.name)
             }
 
+            // solve flowsheet
             cy.solve_flowsheet()
             cy.screenshot("solved "+flowsheet.name)
-            
     
             // Click save configuration button
             cy.findByRole('button', {name: /save configuration/i}).click()
             cy.wait(1000)
             cy.screenshot('pre saveConfig for '+flowsheet.name)
             
-    
             // Clear preset name and enter new name
             cy.wait(1000)
             cy.get('.MuiInput-input').should('be.visible')
             cy.get('.MuiInput-input', {timeout: 10000}).clear({force: true})
             cy.get('.MuiInput-input', {timeout: 10000}).type('new_test_configuration', {force: true})
             cy.screenshot('saveConfig')
-            
     
             // Click on save (config) and wait for api response
             cy.save_configuration()
             cy.screenshot('saved config for '+flowsheet.name)
-            
     
             // Click compare tab
             cy.findByRole('tab', {name: /compare/i}).click()
@@ -137,7 +130,6 @@ describe('WaterTAP UI Testing', () => {
     
             // Verify that new name is shown in comparison table
             cy.findAllByRole('tabpanel', {name: /compare/i})
-    
             cy.screenshot('end-solve-'+flowsheet.name)
         })
     })
@@ -147,6 +139,7 @@ describe('WaterTAP UI Testing', () => {
             cy.load_flowsheets_list()
             cy.screenshot('loaded flowsheet list page')
             
+            // load flowsheet
             cy.load_flowsheet(flowsheet.name)
             cy.screenshot('loaded '+flowsheet.name)
 
@@ -171,7 +164,7 @@ describe('WaterTAP UI Testing', () => {
             cy.wait(5000)
             cy.screenshot('ran parameter sweep '+flowsheet.name)
 
-            //test that sweep was successful
+            // verify that sweep was successful
             cy.get('.parameter-sweep-output-table').should('be.visible')
 
         })
