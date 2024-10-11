d4ff6166a260d1e09e915b62a1516553b53b68f3
MichaelPesce <michaelcpesce@gmail.com>
2024-10-11 08:24:10 -0400

    update version numbers; run 1.1.0 build

--- electron/ui/src/theme.js
+++ electron/ui/src/theme.js
@@ -23,8 +23,8 @@ export const themes = {
             '(WaterTAP) is an open-source Python-based software package that ' +
             'supports the technoeconomic assessment of full water treatment trains.',
         projectRelease: {
-            'version': '24.10.03', 'depVersions': {
-                'WaterTAP': '1.1.0rc0'
+            'version': '24.10.11', 'depVersions': {
+                'WaterTAP': '1.1.0'
             }
         },
         splashImage: nawiSplash,
