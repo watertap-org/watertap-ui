7b205a7e945944b0dfaabe047ab8d57dedebcdf0
MichaelPesce <michaelcpesce@gmail.com>
2024-11-20 14:59:05 -0600

    send project name as argument for deployment

--- backend/tests/app/internal/test_settings.py
+++ backend/tests/app/internal/test_settings.py
@@ -14,17 +14,17 @@ app_log = logging.getLogger("app.internal.settings")
 def test_deployment_valid_projects():
     # use approved project names
     for p in Deployment.PROJ:
-        os.environ[Deployment.PROJECT_ENV] = p
-        d = Deployment()
+        # os.environ[Deployment.PROJECT_ENV] = p
+        d = Deployment(p)
         assert d.project == p
 
 
 def test_deployment_invalid_projects():
     # try bad project names
     for name in "bad", "", "\U0001f600":
-        os.environ[Deployment.PROJECT_ENV] = name
+        # os.environ[Deployment.PROJECT_ENV] = name
         with pytest.raises(ValueError):
-            _ = Deployment()
+            _ = Deployment(name)
 
 
 def test_deployment_env_default():
